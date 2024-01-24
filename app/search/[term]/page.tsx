b92ece8704dcdee41750f3dff2050f96a326dace
Martin Alejandro Mammana <martin.ale590@gmail.com>
2024-01-24 16:19:46 -0300

    [feature] added search for a movie

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,15 +1,25 @@
+import MoviesCarousel from "@/components/MoviesCarousel"
+import { getPopularMovies, getSearchedMovies } from "@/lib/getMovies"
 import { notFound } from "next/navigation"
 
-const SearchPage = ({ params: { term }}: { params: { term: string } }) => {
+const SearchPage = async ({ params: { term }}: { params: { term: string } }) => {
   if(!term) notFound()
 
   const termToUse = decodeURI(term)
 
   //API call to get the searched movies
+  const movies = await getSearchedMovies(termToUse)
   //API call to get the Popular Movies
+  const popularMovies = await getPopularMovies()
 
   return (
-    <div>Welcome to the search page: {termToUse}</div>
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-4 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {termToUse}</h1>
+        <MoviesCarousel title="Movies" movies={movies} isVertical/>
+        <MoviesCarousel title="You may also like" movies={popularMovies} />
+      </div>
+    </div>
   )
 }
 
